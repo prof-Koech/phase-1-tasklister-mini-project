5319ba752cb6b9d6e8786aba77cdb0146d41c063
Kennedy <kiplangatkennedy019@gmail.com>
2023-03-31 12:21:04 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  let form = document.getElementById('create-task-form')
+  form.addEventListener('submit',(e) => {
+    e.preventDefault()
+    createTask(e.target['new-task-description'].value)
+    form.reset()
+  })
 });
+
+
+function createTask (task){
+  let li = document.createElement('li');
+  let button = document.createElement('button')
+  button.addEventListener('click', deleteTask)
+  button.textContent = "delete"
+  li.textContent = task + "  " // this will create space between task and delete button
+  li.appendChild(button)
+//grab tasks by Id where tasks will live on DOM
+  let taskList = document.getElementById("tasks")
+  taskList.appendChild(li)
+}
+
+
+function deleteTask(e){
+  e.target.parentNode.remove()
+}
+
